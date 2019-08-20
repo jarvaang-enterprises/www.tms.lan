87a9411c10a270b73dd2d9919e808cb56ed64f7d
juniorkibirige <aanga26@gmail.com>
2019-08-20 11:38:13 +0300

    Added htaccess to gitignore

--- rk/index.php
+++ rk/index.php
@@ -10,7 +10,7 @@ $acc = 'Accomodation';
     </div>
     <div class="linear-nav" style="display:block;margin-top:15px">
         <li class="navs hotels">
-            <span class="txt">Hotels</span>
+            <span class="txt">Hotel</span>
             <span class="sm-scr">Hot</span>
         </li>
         <hr style="margin:0px 0px;background-color:black">
@@ -20,21 +20,77 @@ $acc = 'Accomodation';
        </li>
         <hr style="margin:0px 0px;">
         <li class="navs accs">
-            <span class="txt">Accomodation</span>
-            <span class="sm-scr">Acc</span>
+            <span class="txt">Aparthotel</span>
+            <span class="sm-scr">Ah</span>
         </li>
         <hr style="margin:0px 0px;">
         <li class="navs tc">
-            <span class="txt">Test Content</span>
-            <span class="sm-scr">TC</span>
+            <span class="txt">Bed and breakfast</span>
+            <span class="sm-scr">Bb</span>
        </li>
         <hr style="margin:0px 0px;">
         <li class="navs tc">
-            <span class="txt">Test Content</span>
-            <span class="sm-scr">TC</span>
+            <span class="txt">Boat</span>
+            <span class="sm-scr">Bt</span>
+       </li>
+        <hr style="margin:0px 0px;">
+        <li class="navs tc">
+            <span class="txt">Campsite</span>
+            <span class="sm-scr">Cs</span>
+       </li>
+        <hr style="margin:0px 0px;">
+        <li class="navs tc">
+            <span class="txt">Chalet</span>
+            <span class="sm-scr">Chal</span>
+       </li>
+        <hr style="margin:0px 0px;">
+        <li class="navs tc">
+            <span class="txt">Country house</span>
+            <span class="sm-scr">Ch</span>
+       </li>
+        <hr style="margin:0px 0px;">
+        <li class="navs tc">
+            <span class="txt">Lodge</span>
+            <span class="sm-scr">Ldg</span>
+       </li>
+        <hr style="margin:0px 0px;">
+        <li class="navs tc">
+            <span class="txt">Motel</span>
+            <span class="sm-scr">Mtl</span>
+       </li>
+        <hr style="margin:0px 0px;">
+        <li class="navs tc">
+            <span class="txt">Resort</span>
+            <span class="sm-scr">Rsrt</span>
+       </li>
+        <hr style="margin:0px 0px;">
+        <li class="navs tc">
+            <span class="txt">Riad</span>
+            <span class="sm-scr">Rd</span>
+       </li>
+        <hr style="margin:0px 0px;">
+        <li class="navs tc">
+            <span class="txt">Ryokan</span>
+            <span class="sm-scr">Rykn</span>
+       </li>
+        <hr style="margin:0px 0px;">
+        <li class="navs tc">
+            <span class="txt">Luxury tent</span>
+            <span class="sm-scr">Lt</span>
+       </li>
+        <hr style="margin:0px 0px;">
+        <li class="navs tc">
+            <span class="txt">Villa</span>
+            <span class="sm-scr">Vla</span>
+       </li>
+        <hr style="margin:0px 0px;">
+        <li class="navs tc">
+            <span class="txt">Other</span>
+            <span class="sm-scr">Ot</span>
        </li>
         <hr style="margin:0px 0px;">
     </div>
+    <optgroup></optgroup>
     <div class="content" style="display: none">
         <div id="hostels" style="display:none">
             This is a list of all hostels in Kampala:
