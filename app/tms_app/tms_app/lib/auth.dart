48b0ef96a2bd70f161654c09c13300f968c8bd52
juniorkibirige <aanga26@gmail.com>
2019-11-13 00:12:30 +0300

    Cleaned unused files

--- app/tms_app/tms_app/lib/auth.dart
+++
@@ -1,9 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-
-class AuthService {
-  static final AuthService _service = new AuthService._internal();
-  AuthService._internal();
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final GoogleSignIn googleSignIn = new GoogleSignIn();
-}
